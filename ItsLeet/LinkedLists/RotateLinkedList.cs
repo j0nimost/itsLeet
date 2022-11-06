5722584432fec0da2f0518868801cbc2a253601d
j0nimost <john.nyingi@live.com>
2022-11-06 19:30:26 +0300

    added a linkedList

---
+++ ItsLeet/LinkedLists/RotateLinkedList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItsLeet.LinkedLists
+{
+    public class RotateLinkedList
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            // rotate by k steps
+            // get last listNode
+            //mark next as head
+
+            List<ListNode> listNodes = new List<ListNode>();
+            // get last node
+
+            if (head == null)
+                return null;
+
+            while (head.next != null)
+            {
+                listNodes.Add(head);
+                head = head.next;
+            }
+            
+            listNodes.Add((ListNode)head);
+
+            // rotate array
+            k = k % listNodes.Count;
+
+            RotateList(listNodes, 0, listNodes.Count-1);
+            RotateList(listNodes, 0, k-1);
+            RotateList(listNodes, k, listNodes.Count-1);
+
+            ListNode resultNode = null;
+
+            for (int i = listNodes.Count-1; i >=0; i--)
+            {
+                listNodes[i].next = null;
+
+                if (resultNode == null)
+                {
+                    resultNode = new ListNode(listNodes[i].val);
+                }
+                else
+                {
+                    ListNode newNode = new ListNode(listNodes[i].val);
+                    newNode.next = resultNode;
+                    resultNode = newNode;
+                }
+            }
+            
+            return resultNode;
+        }
+
+        public static void RotateList(List<ListNode> nodes, int start, int last)
+        {
+            while(last > start)
+            {
+                var temp = nodes[start];
+                nodes[start] = nodes[last];
+                nodes[last] = temp;
+
+                start++;
+                last--;
+            }
+        }
+    }
+}
--- ItsLeet/Program.cs
+++ ItsLeet/Program.cs
@@ -2,22 +2,18 @@
 using ItsLeet.LinkedLists;
 using System.Diagnostics;
 
-
-AddNumbers addNumbers = new AddNumbers();
-
-ListNode list = new ListNode(2);
-list.next = new ListNode(4);
+RotateLinkedList RotateLinkedList = new RotateLinkedList();
+ListNode list = new ListNode(1);
+list.next = new ListNode(2);
 list.next.next = new ListNode(3);
+list.next.next.next = new ListNode(4);
+list.next.next.next.next = new ListNode(5);
 
-ListNode list2 = new ListNode(5);
-list2.next = new ListNode(6);
-list2.next.next  = new ListNode(4);
-
-var result = addNumbers.AddTwoNumbers(list, list2);
+var result = RotateLinkedList.RotateRight(list, 2);
 
 while(result.next != null)
 {
-    Console.Write(result.val);
+    Console.Write(result.val + "->");
     result = result.next;
 }
 Console.Write(result.val);
